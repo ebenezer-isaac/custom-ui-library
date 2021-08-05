17d9d9c8165d4ecab8a4946bc11a5eeb8f41c1e5
Ebenezer Isaac <ebnezr.isaac@gmail.com>
2021-08-05 09:45:54 +0530

    tabs

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import Tooltip from './custom-ui/tooltip'
 import Dropdown from './custom-ui/dropdown'
+import Tabs from './custom-ui/tabs'
 const tooltip = new Tooltip(document.querySelector(".tooltip"));
 const dropdowns = document.querySelectorAll(".dropdown");
+const tab = document.querySelector('.tabs');
 dropdowns.forEach(dropdown=>{
     const instance = new Dropdown(dropdown);
     instance.init();
 })
 tooltip.init();
+const tabs = new Tabs(tab);
+tabs.init();
 
 
