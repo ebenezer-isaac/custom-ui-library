a7282f754c513eb81b94d2888c6a6841e1533e30
Ebenezer Isaac <ebnezr.isaac@gmail.com>
2021-08-05 10:06:45 +0530

    snackbar

--- src/app.js
+++ src/app.js
@@ -1,15 +1,23 @@
 import Tooltip from './custom-ui/tooltip'
 import Dropdown from './custom-ui/dropdown'
 import Tabs from './custom-ui/tabs'
+import Snackbar from './custom-ui/snackbar'
+
 const tooltip = new Tooltip(document.querySelector(".tooltip"));
 const dropdowns = document.querySelectorAll(".dropdown");
 const tab = document.querySelector('.tabs');
-dropdowns.forEach(dropdown=>{
+const snackbar = new Snackbar();
+const tabs = new Tabs(tab);
+const button = document.querySelector('button')
+dropdowns.forEach(dropdown => {
     const instance = new Dropdown(dropdown);
     instance.init();
 })
+button.addEventListener('click', () => {
+    snackbar.show('You clicked me :)')
+})
+snackbar.init();
 tooltip.init();
-const tabs = new Tabs(tab);
 tabs.init();
 
 
--- src/custom-ui/tabs.js
+++ src/custom-ui/tabs.js
@@ -1,5 +1,5 @@
 import './styles/tabs.css'
-
+ 
 class Tabs {
     constructor(container) {
         this.container = container;
