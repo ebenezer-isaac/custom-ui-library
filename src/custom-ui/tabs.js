5325b72994e6702ec27ba1b9a08a3df1edfdad14
Ebenezer Isaac <ebnezr.isaac@gmail.com>
2021-08-05 09:46:28 +0530

    tabs

---
+++ src/custom-ui/tabs.js
@@ -0,0 +1,27 @@
+import './styles/tabs.css'
+
+class Tabs {
+    constructor(container) {
+        this.container = container;
+        this.tabs = container.querySelectorAll('.trigger')
+    }
+
+    init() {
+        this.tabs.forEach(tab => {
+            tab.addEventListener('click', e => {
+                this.toggleTabs(e);
+            })
+        })
+    }
+
+    toggleTabs(e) {
+        this.tabs.forEach(tab => tab.classList.remove("active"))
+        e.target.classList.add("active")
+    }
+
+    toggleContent(e) {
+        this.container.querySelectorAll('.content').forEach(tab => tab.classList.remove("active"))
+        this.container.querySelectorAll(e.target.getAttribute("data-target")).classList.add("active");
+    }
+}
+export {Tabs as default};
